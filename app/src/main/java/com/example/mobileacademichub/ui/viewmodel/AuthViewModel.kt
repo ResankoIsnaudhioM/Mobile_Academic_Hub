d2999ae87697ec39f061da233297a3d6016b7362
ResankoIsnaudhioM <dhioresanko@gmail.com>
2026-05-25 11:45:21 +0700

    tambah lagi

--- app/src/main/java/com/example/mobileacademichub/ui/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/mobileacademichub/ui/viewmodel/AuthViewModel.kt
@@ -5,13 +5,6 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-sealed class AuthState {
-    data object Idle : AuthState()
-    data object Loading : AuthState()
-    data object Success : AuthState()
-    data class Error(val message: String) : AuthState()
-}
-
 class AuthViewModel : ViewModel() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
